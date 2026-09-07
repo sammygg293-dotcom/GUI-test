6c80a5137c608789cee0ba4a4df2d0f413693b39
Author_O_Many <sammygg293@gmail.com>
2026-09-07 12:11:08 +0100

    Update currency-converter.py

--- currency-converter.py
+++ currency-converter.py
@@ -1,5 +1,10 @@
 from tkinter import *
 
+def pounds_to_euros():
+    pounds = txt_pounds.get()
+    euros = float(pounds) * 1.16
+    txt_euros.insert(END,f"{euros:.2f}")
+
 window = Tk()
 window.geometry("200x300")
 
@@ -9,13 +14,13 @@ LBL_pounds.pack()
 txt_pounds = Entry(window, width = 15)
 txt_pounds.pack()
 
-btn_convert = Button(window, text="Convert")
+btn_convert = Button(window, text="Convert", command=pounds_to_euros)
 btn_convert.pack(pady = 10)
 
 LBL_euros = Label(window, text="Euros")
 LBL_euros.pack()
 
-txt_euros = Label(window, width = 15)
+txt_euros = Entry(window, width = 15)
 txt_euros.pack()
 
 window.mainloop()
